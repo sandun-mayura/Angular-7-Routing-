567b7e53713792476d69f292a3cf850605a7943f
Sandun_meyura <iit140013@std.uwu.ac.lk>
2019-01-10 16:17:29 +0530

    no message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import {DepatmentListComponent} from "./depatment-list/depatment-list.component";
 import {EmployeeListComponent} from "./employee-list/employee-list.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {SellsListComponent} from "./sells-list/sells-list.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/department',pathMatch: 'full'},
   {path: 'department', component: DepatmentListComponent},
-  {path: 'department', component: DepatmentListComponent},
+  {path: 'sell', component: SellsListComponent},
   {path: 'employee', component: EmployeeListComponent},
   {path: '**', component: PageNotFoundComponent}
 ];
@@ -17,4 +18,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [DepatmentListComponent, EmployeeListComponent, PageNotFoundComponent]
+export const routingComponents = [DepatmentListComponent,SellsListComponent, EmployeeListComponent, PageNotFoundComponent]
